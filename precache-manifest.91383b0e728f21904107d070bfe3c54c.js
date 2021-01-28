b39e1aa2c58bb891086f0dc13ebc6c9169b79e1a
zhijiang wang <wangzhijiang333@icloud.com>
2021-01-28 13:10:04 +1000

    new  commit

---
+++ precache-manifest.91383b0e728f21904107d070bfe3c54c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b06fdd92cb9bbdadc093abff8404f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53447173856e48a3ddd",
+    "url": "/static/css/main.3204799d.chunk.css"
+  },
+  {
+    "revision": "9ebab8ceb98888620337",
+    "url": "/static/js/2.7b30ef16.chunk.js"
+  },
+  {
+    "revision": "3ef73e5e5db1baa200030938167dc40b",
+    "url": "/static/js/2.7b30ef16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53447173856e48a3ddd",
+    "url": "/static/js/main.1815948b.chunk.js"
+  },
+  {
+    "revision": "b8433edc1e70d2d6110d",
+    "url": "/static/js/runtime-main.102d1640.js"
+  },
+  {
+    "revision": "0b823006c32aaea6c91be5583357e256",
+    "url": "/static/media/Nodata.0b823006.png"
+  },
+  {
+    "revision": "f0ab0719a208f4c97d5d316dfa178890",
+    "url": "/static/media/about.f0ab0719.jpg"
+  },
+  {
+    "revision": "2ddad1e845abc1b377dc55b9f8cc8d64",
+    "url": "/static/media/home.2ddad1e8.jpg"
+  },
+  {
+    "revision": "2ed5d8bfcaf40f8316c43199a82fcede",
+    "url": "/static/media/logo.2ed5d8bf.png"
+  }
+]);
